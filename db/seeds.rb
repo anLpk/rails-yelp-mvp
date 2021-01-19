011e76e249fc514328be92dd1b52f17a7b60c90b
anil pak <anilpak35@gmail.com>
2021-01-19 19:22:33 +1100

    Seeded 5 restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,48 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+category = %w[chinese italian japanese french belgian]
+puts "cleaning up database"
+Restaurant.destroy_all
+
+puts "database is clean"
+
+puts 'Creating 5 restaurants...'
+
+restaurants_att = [
+  {
+    name:         'Chin Chin',
+    address:      '125 Flinders Ln, Melbourne VIC 3000',
+    phone_number: '0386632000',
+    category:     category.sample
+  },
+
+  {
+    name:         'Tulum',
+    address:      '217 Carlisle St, Balaclava VIC 3183',
+    phone_number: '0424131881',
+    category:     category.sample
+  },
+
+  {
+    name:         'Maha',
+    address:      '21 Bond St, Melbourne VIC 3000',
+    phone_number: '0396295900',
+    category:     category.sample
+  },
+
+  {
+    name:         'Carretão',
+    address:      '34 Beach St, 56739 São Paolo',
+    phone_number: '9823457380',
+    category:     category.sample
+  },
+
+  {
+    name:         'Lala Land',
+    address:      '50 Church St, 3546 Richmond',
+    phone_number: '87348230210',
+    category:     category.sample
+  }
+
+]
+
+Restaurant.create!(restaurants_att)
+puts 'Finished!'
